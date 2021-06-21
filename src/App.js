0b6c0b416e25a2a7c62e425ec9d6d8f822d3372a
Christopher Sy Alba <albasychris@gmail.com>
2021-06-22 11:39:02 +1200

    setup themes basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react"
+// 1: Import
+import React from 'react';
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyles } from './themes/globalStyles';
+import themes from "./themes/schema.json";
+// 2: Create a cotainer
+const Container = styled.div`
+  margin: 5px auto 5px auto;
+`;
 
 const App = () => {
+  const selectedTheme = themes.data.seaWave
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <ThemeProvider theme={selectedTheme}>
+      <GlobalStyles />
+      <Container style={{ fontFamily: selectedTheme.font }}>
+        <h1>Theme Builder</h1>
+        <p>
+          This is a theming system with a Theme Switcher and Theme Builder.
+          Do you want to see the source code? <a href="https://github.com/atapas/theme-builder" target="_blank" rel="noreferrer">Click here.</a>
+        </p>
+      </Container>
+    </ThemeProvider>
   );
 }
 
