f27963f0f49c65ad0b2d1c76c8f8a018f81ef441
Christopher <calb200@aucklanduni.ac.nz>
2021-06-22 22:30:52 +1200

    setup categories basic

---
+++ src/Pages/Home/Categories/Categories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { JCUXTitle } from "../../../Components/JCUX/JCUXTitle";
+import { CategoriesOuterBox } from "./styled";
+
+const Categories = () => {
+  return (
+    <CategoriesOuterBox>
+      <JCUXTitle>CATEGORIES</JCUXTitle>
+    </CategoriesOuterBox>
+  );
+};
+
+export default Categories;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Categories from "./Categories";
 import Hero from "./Hero";
 import TopPicks from "./TopPicks";
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Hero />
       <TopPicks />
+      <Categories />
     </>
   );
 };
