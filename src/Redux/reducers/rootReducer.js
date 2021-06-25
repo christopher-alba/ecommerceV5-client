6bcb7965324068821dbe7dedd64408e2b19d2821
Christopher <calb200@aucklanduni.ac.nz>
2021-06-25 14:53:01 +1200

    add cart reducer to root reducers

--- src/Redux/reducers/rootReducer.js
+++ src/Redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { cartReducer } from "./cart";
 import { shopReducer } from "./shop";
 
 const rootReducer = combineReducers({
   shop: shopReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
