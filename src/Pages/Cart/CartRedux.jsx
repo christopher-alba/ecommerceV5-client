fcdf8de7bece1ee2d89961cd0c5c069c4a66ed49
Christopher <calb200@aucklanduni.ac.nz>
2021-06-25 14:59:35 +1200

    connect cartRedux component to redux store

--- src/Pages/Cart/CartRedux.jsx
+++ src/Pages/Cart/CartRedux.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import { clearCart, removeFromCart } from "../../Redux/actions/cart";
 
-const CartRedux = () => {
-  return <h1>CART REDUX</h1>;
+const CartRedux = ({ products, clearCart, removeFromCart }) => {
+  return <h1>{JSON.stringify(products)}</h1>;
 };
-export default CartRedux;
+
+const mapStateToProps = (state) => {
+  return {
+    products: state.cart.products,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearCart: () => {
+      dispatch(clearCart());
+    },
+    removeFromCart: (productId) => {
+      dispatch(removeFromCart(productId));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartRedux);
